c133c6a796b01e4fa9b0a092b9145c30554ffa53
Ricardo A. Morales R <ricaozmorales@gmail.com>
2016-06-10 12:53:20 -0600

    version 2

--- js/modulo.js
+++ js/modulo.js
@@ -2,6 +2,7 @@ angular.module("modulo1",[])
 .controller("c1",function($scope){
 	$scope.edad=10;
 	$scope.edad2=20;
+
 	$scope.arreglo=[{nombre:"juan",edad:11},
 	{nombre:"pedro",edad:131},
 	{nombre:"jsesaea",edad:13}];
@@ -15,9 +16,25 @@ angular.module("modulo1",[])
 
 
 
-}).controller("c2",function($scope){
+}).controller("c2",function($scope,$http){
 	$scope.edad=200;
 	$scope.edad2=200;
-
+	$http.get("http://jsonplaceholder.typicode.com/posts")
+	.success(function(data){
+		$scope.arreglo=data;
+	})
+	.error(function(data){
+		console.log(data);
+	});
+	$scope.agregar = function(){
+		$http.post("http://jsonplaceholder.typicode.com/posts",$scope.nuevo)
+		.success(function(data){
+			$scope.arreglo.push(data);
+		})
+		.error(function(data){
+			console.log(data);
+		});
+		$scope.nuevo={};
+	}
 })
 ;
